4983e4a948054c1a2fc87656e80e5e0f09a7949a
Douglas Lins <douglas.lins@ifood.com.br>
2021-05-06 22:10:56 -0300

    renomeado a o pacote resource para controller

--- src/main/java/br/com/lins/dscatalog/controller/CategoryController.java
+++ src/main/java/br/com/lins/dscatalog/controller/CategoryController.java
@@ -1,4 +1,4 @@
-package br.com.lins.dscatalog.resources;
+package br.com.lins.dscatalog.controller;
 
 import java.net.URI;
 import java.util.List;
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +18,7 @@ import br.com.lins.dscatalog.services.CategoryService;
 
 @RestController
 @RequestMapping(value = "/categories")
-public class CategoryResource {
+public class CategoryController {
 	
 	@Autowired
 	private CategoryService service;
